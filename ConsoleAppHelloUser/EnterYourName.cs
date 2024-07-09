129d1ff215c85d0cfe97167d4ce42118e745bb9d
Dzmitry Skryliou <dzmitry_skryliou@epam.com>
2024-07-09 16:38:10 +0200

    Add project files.

---
+++ ConsoleAppHelloUser/EnterYourName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleAppHelloUser
+
+{
+    public class EnterYourName
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter your username please: ");
+            string? username = Console.ReadLine();
+
+            HelloWorld helloWorld = new HelloWorld();
+            string message = helloWorld.HelloMessage(username);
+
+            Console.WriteLine(message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorldLibrary/HelloWorld.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleAppHelloUser
+{
+    public class HelloWorld
+    {
+        public string HelloMessage(string username)
+        {
+            string currentTime = DateTime.Now.ToString();
+            return $"{currentTime} Hello, {username}!";
+        }
+    }
+}
